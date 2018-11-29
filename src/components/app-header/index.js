f02e6defde13c668ba5aa6cb4fe8f55217d2981d
avtarSoft <avtarsohi11@gmail.com>
2018-11-29 11:16:35 +0530

    logo issue fixed

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 export default function AppHeader() {
   return (
     <div className="__starwar__App-header">
-      <img src={logo} className="__starwar__App-logo" alt="logo" />
+      <img
+        src="https://rawcdn.githack.com/avtarSoft/starwar/7da2dc4da8123e56f7dd5e99719bcf4d232947e4/src/logo.svg"
+        className="__starwar__App-logo"
+        alt="logo"
+      />
       <span className="__starwar__App-header-text">
         Star Wars Character Search
       </span>
